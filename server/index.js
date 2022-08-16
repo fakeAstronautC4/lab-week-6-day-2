90aa4928f559f861351bd486ffe41e3e337888e0
fakeAstronautC4 <alonsocamilo609@gmail.com>
2022-08-16 15:14:51 -0400

    second attempt

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const app = express()
 // app.use(cors())
 // app.use(express.json())
 const port = process.env.PORT || 4000
-
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname, '../index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server running on ${port}`)
