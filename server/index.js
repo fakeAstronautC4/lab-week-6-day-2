1c47a256f558e898b071df3a939910dcf9de50d6
fakeAstronautC4 <alonsocamilo609@gmail.com>
2022-08-16 15:23:36 -0400

    third attempt

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-const express = require("express")
-// const cors = require("cors")
-const path = require("path")
+const express = require('express')
+const path = require('path')
+
 const app = express()
-// app.use(cors())
-// app.use(express.json())
-const port = process.env.PORT || 4000
+
 app.get('/',function(req,res) {
-    res.sendFile(path.join(__dirname, '../index.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
+const port = process.env.PORT || 4005
+// This will get the PORT variable from Heroku. However, if one isn't assigned (ex. when we are testing locally) it will use port 4005.
+
 app.listen(port, () => {
-    console.log(`Server running on ${port}`)
+  console.log(`Listening on port ${port}`)
 })
