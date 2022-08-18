a8bd58dcd0c76c8a53b82900b0f535103ffae80a
fakeAstronautC4 <alonsocamilo609@gmail.com>
2022-08-18 14:22:48 -0400

    rollbar stufsecond stuff

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ const express = require('express')
 const path = require('path')
 
 
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
-  accessToken: '767194a84a3344ad8ee870bebb34ef15',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-})
+// var Rollbar = require('rollbar')
+// var rollbar = new Rollbar({
+//   accessToken: '767194a84a3344ad8ee870bebb34ef15',
+//   captureUncaught: true,
+//   captureUnhandledRejections: true,
+// })
 
 // record a generic message and send it to Rollbar
-rollbar.log('Hello world!')
+// rollbar.log('Hello world!')
 
 const app = express()
 
